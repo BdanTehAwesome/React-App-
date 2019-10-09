21c94becfc6f137f5851c852be3c74701ba04c81
Brendan Delaney <brendandelaneyirl@gmail.com>
2019-10-09 22:45:58 +0100

    connecting to firebase success

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -16,25 +16,24 @@ const firebaseConfig = {
   class Firebase {
     constructor() {
       app.initializeApp(firebaseConfig);
-
+  
       this.auth = app.auth();
-      this.db = app.database();
     }
+  
+    // *** Auth API ***
+  
     doCreateUserWithEmailAndPassword = (email, password) =>
-    this.auth.createUserWithEmailAndPassword(email, password);
-
+      this.auth.createUserWithEmailAndPassword(email, password);
+  
     doSignInWithEmailAndPassword = (email, password) =>
-    this.auth.signInWithEmailAndPassword(email, password);
-
+      this.auth.signInWithEmailAndPassword(email, password);
+  
     doSignOut = () => this.auth.signOut();
-
+  
     doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-    
+  
     doPasswordUpdate = password =>
       this.auth.currentUser.updatePassword(password);
-    
-    // *** User API ***
-    user = uid => this.db.ref(`users/${uid}`);
-    users = () => this.db.ref('users');
   }
+  
   export default Firebase;

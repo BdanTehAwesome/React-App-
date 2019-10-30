c9e0ab61d53c08e6e6e66c064a6280d8f7b06898
Brendan Delaney <brendandelaneyirl@gmail.com>
2019-10-30 01:23:00 +0000

    PROTECTED ROUTES added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
-const App = () => (
+
+import {withAuthorization} from '../Session';
+
+const HomePage = () => (
   <div>
     <h1>We Be Home</h1>
+    <p>The home page is accessible from signed in users</p>
   </div>
 );
-export default App;
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(HomePage);
